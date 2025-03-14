a06ab3950bc4616c88970e6ba628b08e4b7b0800
Emmanuel Vidal <emmanuelv02@hotmail.com>
2025-03-14 01:27:51 -0400

    Implement turbo base project structure

--- apps/inventory-service/src/modules/inventory/tests/inventory.controller.spec.ts
+++ apps/inventory-service/src/modules/inventory/tests/inventory.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { InventoryController } from './inventory.controller';
+import { InventoryController } from '../inventory.controller';
 
 describe('InventoryController', () => {
   let controller: InventoryController;
